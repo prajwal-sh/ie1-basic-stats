aee482bd2ee3d15a79ce60ea780427b935435af0
Prajwal <prajwalsh17@outlook.com>
2023-10-02 21:53:44 -0400

    Including the logic for computing maximum

--- src/MinMaxCalculation.java
+++ src/MinMaxCalculation.java
@@ -1,14 +1,20 @@
 public class MinMaxCalculation {
-     public static double minmax(double ... numbers) {
+     public static double[] minmax(double ... numbers) {
+        double sum = 0;
         double minimum=numbers[0];
+        double maximum=numbers[0];
         for (int i=0;i<numbers.length;i++) {
             if(numbers[i]<minimum)
             {
                 minimum=numbers[i];
             }
-
+            else if(numbers[i]>maximum)
+            {
+                maximum=numbers[i];
+            }
         }
-        return minimum;
+        double[] result={minimum,maximum};
+        return result;
     }
     
 }
