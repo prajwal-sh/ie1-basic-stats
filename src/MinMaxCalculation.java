b3743b64d47dced56bc9d5dc1b2c3d3fe61c0e78
prajwal <prajwalsh17@outlook.com>
2023-10-03 19:52:10 -0400

    Added comments and modularity

--- src/MinMaxCalculation.java
+++ src/MinMaxCalculation.java
@@ -1,20 +1,36 @@
 public class MinMaxCalculation {
-     public static double[] minmax(double ... numbers) {
-        double sum = 0;
-        double minimum=numbers[0];
-        double maximum=numbers[0];
-        for (int i=0;i<numbers.length;i++) {
-            if(numbers[i]<minimum)
-            {
-                minimum=numbers[i];
-            }
-            else if(numbers[i]>maximum)
-            {
-                maximum=numbers[i];
+    public static double min(double... numbers) {
+        if (numbers.length == 0) {
+            // Checks if double numbers array is empty, if yes, throws an exception
+            throw new IllegalArgumentException("Please enter valid numbers as input. Null array not accepted ");
+        }
+
+        double minimum = numbers[0]; // Initializing minimum as the first element in the array
+
+        for (int i = 0; i < numbers.length; i++) {
+            // If the number at the i-th index is smaller than minimum
+            if (numbers[i] < minimum) {
+                minimum = numbers[i]; // Update the minimum
             }
         }
-        double[] result={minimum,maximum};
-        return result;
+        return minimum; // Returning the minimum value
+    }
+
+    public static double max(double... numbers) {
+       if (numbers.length == 0) {
+            // Checks if double numbers array is empty, if yes, throws an exception
+            throw new IllegalArgumentException("Please enter valid numbers as input. Null arrays not accepted");
+        }
+
+        double maximum = numbers[0]; // Initializing maximum as the first element in the array
+
+        for (int i = 0; i < numbers.length; i++) {
+            // If the number at the i-th index is greater than maximum
+            if (numbers[i] > maximum) {
+                maximum = numbers[i]; // Update the maximum
+            }
+        }
+        return maximum; // Returning the maximum value
     }
     
 }
